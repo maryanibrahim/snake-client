232647103743c27c9b706eeec1ceae48ec13ce85
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-14 04:51:19 +0000

    update

--- client.js
+++ client.js
@@ -14,6 +14,16 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: RMR');
+    /* conn.write('Move: up');
+    setTimeout(() => {
+      conn.write('Move: down');
+    }, 500); // Delay sending the "Move: down" command by 500 milliseconds
+    setTimeout(() => {
+      conn.write('Move: left');
+    }, 1000); // Delay sending the "Move: left" command by 1000 milliseconds
+    setTimeout(() => {
+      conn.write('Move: right');
+    }, 1500); // Delay sending the "Move: right" command by 1500 milliseconds */
   });
 
   // Event handler for incoming data from the server
