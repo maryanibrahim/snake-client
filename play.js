dd07746bd2628856c7d8316f265f98c1935e2608
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-14 06:24:44 +0000

    update

--- play.js
+++ play.js
@@ -1,16 +1,17 @@
-/* eslint-disable no-shadow */
 /* eslint-disable func-names */
 /* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-const { connect } = require('./client'); // Import the connect function from client.js
+const { connect } = require('./client');
 
 console.log('Connecting...');
-const conn = connect(); // Connect to the game server
+const conn = connect();
 
 // Keyboard input handling
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit(); // Exit the game if the user presses Ctrl + C
+  } else if (key === 'n') {
+    const name = 'RMR';
+    conn.write(`Name: ${name}`); // Send the name message to the server
   }
 };
 
@@ -19,16 +20,6 @@ const setupInput = function () {
   stdin.setRawMode(true); // Enable raw mode to handle keyboard inputs
   stdin.setEncoding('utf8'); // Interpret keyboard inputs as UTF-8 strings
   stdin.resume(); // Resume stdin to begin receiving input
-
-  // Event handler for keyboard input
-  const handleUserInput = function (key) {
-    if (key === '\u0003') {
-      process.exit(); // Exit the game if the user presses Ctrl + C
-    } else if (key === 'n') {
-      const name = 'RMR';
-      conn.write(`Name: ${name}`); // Send the name message to the server
-    }
-  };
 
   stdin.on('data', handleUserInput);
 
