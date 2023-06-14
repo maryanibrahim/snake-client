e780d11bdd18a04eb0c53048efc3ba47eb7231da
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-14 06:52:21 +0000

    update

--- input.js
+++ input.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-undef */
 /* eslint-disable func-names */
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit(); // Exit the game if the user presses Ctrl + C
+  } else if (key === 'n') {
+    const name = 'RMR';
+    conn.write(`Name: ${name}`); // Send the name message to the server
   }
 };
 
