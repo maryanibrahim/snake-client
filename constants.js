23a018d1bcca7e854e474a08defeb5b4da93a262
maryanibrahim <maryanibrahim301@yahoo.com>
2023-06-24 04:59:52 +0000

    constants

---
+++ constants.js
@@ -0,0 +1,20 @@
+const IP = '10.0.2.15';
+const PORT = 50541;
+
+const w = 'Move: up';
+const a = 'Move: left';
+const s = 'Move: down';
+const d = 'Move: right';
+const z = 'Say: Hello!';
+const x = 'Say: Goodbye!';
+
+module.exports = {
+  IP,
+  PORT,
+  w,
+  a,
+  s,
+  d,
+  z,
+  x,
+};
